b2505b9446cdc5baa09ea536758e30558131007d
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2021-12-30 18:10:14 +0100

    Added backend to deal with Bat/AC power profiles
    - Allow for automatic switching between power profiles depending on power source
    - Uses udev to trigger a systemd service

---
+++ powersave.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copy Python files to opt directory
+SCRIPT_PATH=$(dirname $(realpath -s $0))
+
+cp "${SCRIPT_PATH}/config.py" "/opt/linux-undervolt/"
+cp "${SCRIPT_PATH}/udev_change.py" "/opt/linux-undervolt/"
+
+# Move temp service to Systemd services
+mv "${1}/temp.service" "/etc/systemd/system/linux-undervolt.powersave.service"
+mv "${1}/temp.env" "/etc/systemd/system/linux-undervolt.powersave.env"
+
+# Move temp rule to Udev rules
+mv "${1}/linux-undervolt.temp" "/etc/udev/rules.d/100.linux-undervolt.rules"
