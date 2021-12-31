aa76762bd7f0370b10701eb8733733f875f2c925
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2021-12-31 17:32:43 +0100

    fixed udevRule function
    - the previous iteration would write the systemd service keys in lowercase
    - fixed the udev rule (removed unecessary double quotes at the end)
    - reload the udev rules after the rule is added

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ HOME = os.environ['HOME']
 
 def createUdevRule(options: dict) -> None:
     """
-    Creaes a Udev rule for when the system is switched between battery and AC power.
+    Creates a Udev rule for when the system is switched between battery and AC power.
     """
     # First we create the systemd service to be called when the udev rule is triggered
 
@@ -17,6 +17,10 @@ def createUdevRule(options: dict) -> None:
         with open(filedir, 'w') as service_file:
     
             config = ConfigParser()
+            
+            # The default optionxform function converts keys to lowercase when writing. This skips that step.
+            config.optionxform = str
+
             config["Unit"] = {
                 "Description": "Toggle Power Profiles on Power change"
             }
@@ -28,20 +32,20 @@ def createUdevRule(options: dict) -> None:
                 "ExecStop": f"/usr/bin/python3 /opt/linux-undervolt/udev_change.py --set-profile {options['ac']}"
             }
             config['Install'] = {"WantedBy": "multi-user.target"}
-
+            
             config.write(service_file, space_around_delimiters=False)
 
         # Add environment variables since the service will be run as root
         env_dir = os.path.join(tmp_dir, "temp.env")
         with open(env_dir, 'w') as env_file:
             env_file.write(f'HOME={HOME}\n')
-            env_file.write(f"XDG_RUNTIME_DIR={TEMP_FILE_DIR}")
+            env_file.write(f"XDG_RUNTIME_DIR={os.environ['XDG_RUNTIME_DIR']}")
 
         # Next create the Udev rule
         temp_rule = os.path.join(tmp_dir, "linux-undervolt.temp")
         with open(temp_rule, 'w') as udev_rule:
 
-            bat = f'ACTION=="change", SUBSYSTEM=="power_supply", ENV{{POWER_SUPPLY_ONLINE}}=="0", RUN+="/usr/bin/systemctl start linux-undervolt.powersave""'
+            bat = f'ACTION=="change", SUBSYSTEM=="power_supply", ENV{{POWER_SUPPLY_ONLINE}}=="0", RUN+="/usr/bin/systemctl start linux-undervolt.powersave"'
             ac = f'ACTION=="change", SUBSYSTEM=="power_supply", ENV{{POWER_SUPPLY_ONLINE}}=="1", RUN+="/usr/bin/systemctl stop linux-undervolt.powersave"'
             udev_rule.write(bat)
             udev_rule.write("\n")
@@ -49,6 +53,6 @@ def createUdevRule(options: dict) -> None:
 
         # Run the shell script to move and copy the files to their appropriate places
         script_file = os.path.join(os.getcwd(), 'powersave.sh')
-        command_run = subprocess.run(f"pkexec sh -c 'chmod +x {script_file}; {script_file} {tmp_dir}'", shell=True)
+        command_run = subprocess.run(f"pkexec sh -c 'chmod +x {script_file}; {script_file} {tmp_dir}; udevadm control --reload'", shell=True)
     
     return command_run
