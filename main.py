a45cab65ee8a0da65de6726d7c2c2dec742996a9
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2022-01-02 14:44:17 +0100

    Improved UI + added functionality to profile dropdown

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ class MainWindow:
 
         # Initial setup
         self.scaleChange()
+        self.getPowerProfiles()
         
         # Set correct profile button active
         button_id = f"profile_{self.config.active_profile}_button"
@@ -52,6 +53,61 @@ class MainWindow:
         window = self.builder.get_object("Main")
         window.connect("delete-event", gtk.main_quit)
         window.show()
+
+    def firstTimeSetup(self):
+        """
+        Creates the initial files required for the application to run. This is only called the first time
+        the application is run. 
+        """
+
+        # Check whether intel-undervolt has been installed.
+
+        prereqs = config.checkPrerequisites()
+        if prereqs:
+            default_undervolt_path = '/etc/intel-undervolt.conf'
+            
+            if os.path.isfile(default_undervolt_path):
+                self.config = config.Config()
+                return gtk.ResponseType.OK
+
+            else:
+                # TODO: Add file chooser dialog for undervolt conf file
+                folder_dialog = gtk.FileChooserDialog("Select intel-undervolt Config File", None, gtk.FileChooserAction.OPEN,
+                (gtk.STOCK_CLOSE, gtk.ResponseType.CLOSE, gtk.STOCK_OPEN, gtk.ResponseType.OK))
+        
+                filter_conf = gtk.FileFilter()
+                filter_conf.set_name("Configuration files")
+                filter_conf.add_pattern("*.conf")
+                folder_dialog.add_filter(filter_conf)
+
+                response = folder_dialog.run()
+
+                if response == gtk.ResponseType.OK:
+                    response = folder_dialog.get_filename()
+                    options = {'undervolt_path': response}
+                    self.config = config.Config(options=options)
+                else:
+                    folder_dialog.destroy()
+
+                return response
+        else:
+            # TODO: Create an error dialog letting the user know what isn't installed.
+            
+            dialog = gtk.MessageDialog(
+                message_type=gtk.MessageType.ERROR,
+                buttons=gtk.ButtonsType.CLOSE,
+                text="""The prerequisites for this program have not been met. Please check whether you have the
+                required programs (intel-undervolt) properly installed."""
+            )
+
+            dialog.run()
+            dialog.destroy()
+
+            return dialog
+
+    ##############
+    # UI Changes #
+    ##############
 
     def scaleChange(self):
         """
@@ -78,10 +134,40 @@ class MainWindow:
         for option in group_list:
             if option.get_active():
                 new_profile = option.get_label()
-                self.config.changeProfile(new_profile)
+                self.config.changeSettings('profile', new_profile)
                 break
 
         self.scaleChange()
+
+    def getPowerProfiles(self):
+        """
+        docstring
+        """
+
+        settings = self.config.getSettings()
+
+        bat_profile = settings['battery_profile']
+        ac_profile = settings['ac_profile']
+
+        if bat_profile:
+            bat_profile = int(bat_profile)
+            bat_profile_dropdown = self.builder.get_object("bat_profile")
+            bat_profile_check = self.builder.get_object("bat_profile_bool")
+
+            bat_profile_dropdown.set_active(bat_profile)
+            bat_profile_check.set_active(True)
+
+        if ac_profile:
+            ac_profile = int(ac_profile)
+            ac_profile_dropdown = self.builder.get_object("ac_profile")
+            ac_profile_check = self.builder.get_object("ac_profile_bool")
+
+            ac_profile_dropdown.set_active(ac_profile)
+            ac_profile_check.set_active(True)
+
+    #####################
+    # Profile Functions #
+    #####################
 
     def saveProfile(self, _):
 
@@ -120,57 +206,35 @@ class MainWindow:
             )
 
         dialog.run()
-        dialog.destroy()
+        dialog.destroy() 
 
-    def firstTimeSetup(self):
+    def setPowerProfile(self, _):
         """
-        Creates the initial files required for the application to run. This is only called the first time
-        the application is run. 
+        docstring
         """
 
-        # Check whether intel-undervolt has been installed.
+        options = {
+            "ac_profile": "",
+            "battery_profile": ""
+        }
 
-        prereqs = config.checkPrerequisites()
-        if prereqs:
-            default_undervolt_path = '/etc/intel-undervolt.conf'
-            
-            if os.path.isfile(default_undervolt_path):
-                self.config = config.Config()
-                return gtk.ResponseType.OK
+        ac_power_bool = self.builder.get_object("ac_profile_bool")
+        bat_power_bool = self.builder.get_object("bat_profile_bool")
 
-            else:
-                # TODO: Add file chooser dialog for undervolt conf file
-                folder_dialog = gtk.FileChooserDialog("Select intel-undervolt Config File", None, gtk.FileChooserAction.OPEN,
-                (gtk.STOCK_CLOSE, gtk.ResponseType.CLOSE, gtk.STOCK_OPEN, gtk.ResponseType.OK))
+        if ac_power_bool.get_active():
+            ac_power_profile = self.builder.get_object("ac_profile")
+            ac_profile = ac_power_profile.get_active()
+            options['ac_profile'] = str(ac_profile)
         
-                filter_conf = gtk.FileFilter()
-                filter_conf.set_name("Configuration files")
-                filter_conf.add_pattern("*.conf")
-                folder_dialog.add_filter(filter_conf)
+        if bat_power_bool.get_active():
+            bat_power_profile = self.builder.get_object("bat_profile")
+            bat_profile = bat_power_profile.get_active()
+            options['battery_profile'] = str(bat_profile)
 
-                response = folder_dialog.run()
+        self.config.changeSettings(options)
+        
 
-                if response == gtk.ResponseType.OK:
-                    response = folder_dialog.get_filename()
-                    self.config = config.Config(undervolt_file=response)
-                else:
-                    folder_dialog.destroy()
 
-                return response
-        else:
-            # TODO: Create an error dialog letting the user know what isn't installed.
-            
-            dialog = gtk.MessageDialog(
-                message_type=gtk.MessageType.ERROR,
-                buttons=gtk.ButtonsType.CLOSE,
-                text="""The prerequisites for this program have not been met. Please check whether you have the
-                required programs (intel-undervolt) properly installed."""
-            )
-
-            dialog.run()
-            dialog.destroy()
-
-            return dialog 
 
 if __name__ == "__main__":
     
