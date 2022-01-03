7a0a8af80bf24ba9b9f7e5c637fcc5d8375f9aa6
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2022-01-03 14:25:53 +0100

    Added profile switching functionality
    - Creates a profile switch rule when 'Set Power Profile' button is used.
    - Requires a battery profile to be set if an AC profile being set.
    - Disables all power profile settings if switch is not turned on.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
@@ -6,6 +7,7 @@ from time import sleep
 import os
 
 import config
+import backend
 
 
 class MainWindow:
@@ -23,7 +25,6 @@ class MainWindow:
         # Define instance variables
         self.config = None
         self.builder = gtk.Builder()
-        self.active_profile = None
 
         # Check if this is the first time the app is run
         if not os.path.isfile(config.CONFIG_DIRECTORY):
@@ -35,13 +36,15 @@ class MainWindow:
             self.config = config.Config(created=True)
 
         # Create template and add general signals 
-        gladeFile = "GUI.glade"
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        gladeFile = os.path.join(current_directory, "GUI.glade")
         
         self.builder.add_from_file(gladeFile)
         self.builder.connect_signals(self)
 
         # Initial setup
         self.scaleChange()
+        self.powerSwitch()
         self.getPowerProfiles()
         
         # Set correct profile button active
@@ -71,7 +74,6 @@ class MainWindow:
                 return gtk.ResponseType.OK
 
             else:
-                # TODO: Add file chooser dialog for undervolt conf file
                 folder_dialog = gtk.FileChooserDialog("Select intel-undervolt Config File", None, gtk.FileChooserAction.OPEN,
                 (gtk.STOCK_CLOSE, gtk.ResponseType.CLOSE, gtk.STOCK_OPEN, gtk.ResponseType.OK))
         
@@ -90,9 +92,8 @@ class MainWindow:
                     folder_dialog.destroy()
 
                 return response
+        
         else:
-            # TODO: Create an error dialog letting the user know what isn't installed.
-            
             dialog = gtk.MessageDialog(
                 message_type=gtk.MessageType.ERROR,
                 buttons=gtk.ButtonsType.CLOSE,
@@ -124,24 +125,9 @@ class MainWindow:
             value = int(value)
             scale.set_value(value)
 
-    def changeProfile(self, widget):
-        """
-        Change the undervolt profile based on which radiobutton is selected. Also redraws the scales based on the new
-        profile's settings.
-        """
-        group_list = widget.get_group()
-
-        for option in group_list:
-            if option.get_active():
-                new_profile = option.get_label()
-                self.config.changeSettings('profile', new_profile)
-                break
-
-        self.scaleChange()
-
     def getPowerProfiles(self):
         """
-        docstring
+        Change power profile dropdowns to settings value.
         """
 
         settings = self.config.getSettings()
@@ -165,9 +151,51 @@ class MainWindow:
             ac_profile_dropdown.set_active(ac_profile)
             ac_profile_check.set_active(True)
 
-    #####################
-    # Profile Functions #
-    #####################
+    def powerSwitch(self):
+        """
+        Activate or deactivate the power profile options according to settings.
+        """
+
+        active = self.config.parser.getboolean('SETTINGS', 'battery_switch')
+
+
+        if active:
+            
+            switch = self.builder.get_object('power_switch')
+            switch.set_active(True)
+
+            objects = ['ac_profile_box', 'bat_profile_box', 'power_profile_button']
+            for object in objects:
+                cur_obj = self.builder.get_object(object)
+                cur_obj.set_sensitive(True)
+
+    ####################
+    # Config Functions #
+    ####################
+
+    def powerProfileActivate(self, _, state):
+        """
+        docstring
+        """
+        state = str(state).lower()
+        self.config.changeSettings('battery_switch', state)
+        self.powerSwitch()
+
+    def changeProfile(self, widget):
+        """
+        Change the undervolt profile based on which radiobutton is selected. Also redraws the scales based on the new
+        profile's settings.
+        """
+        
+        group_list = widget.get_group()
+
+        for option in group_list:
+            if option.get_active():
+                new_profile = option.get_label()
+                self.config.changeSettings('profile', new_profile)
+                break
+
+        self.scaleChange()
 
     def saveProfile(self, _):
 
@@ -218,20 +246,36 @@ class MainWindow:
             "battery_profile": ""
         }
 
-        ac_power_bool = self.builder.get_object("ac_profile_bool")
-        bat_power_bool = self.builder.get_object("bat_profile_bool")
+        ac_power_bool: bool = self.builder.get_object("ac_profile_bool").get_active()
+        bat_power_bool: bool = self.builder.get_object("bat_profile_bool").get_active()
 
-        if ac_power_bool.get_active():
+        backend_dict = {}
+
+        # Battery power profile is required to set profiles
+        if ac_power_bool and not bat_power_bool:
+
+            error = gtk.MessageDialog(
+                message_type=gtk.MessageType.ERROR,
+                buttons=gtk.ButtonsType.OK,
+                text="Battery Profile is required to set an AC profile"
+            )
+            return
+
+        if ac_power_bool:
             ac_power_profile = self.builder.get_object("ac_profile")
             ac_profile = ac_power_profile.get_active()
             options['ac_profile'] = str(ac_profile)
+            backend_dict['ac'] = str(ac_profile)
         
-        if bat_power_bool.get_active():
+        if bat_power_bool:
             bat_power_profile = self.builder.get_object("bat_profile")
             bat_profile = bat_power_profile.get_active()
             options['battery_profile'] = str(bat_profile)
+            backend_dict['bat'] = str(bat_profile)
 
         self.config.changeSettings(options)
+        backend.createUdevRule(backend_dict)
+        
         
 
 
