ae33c60e8f90ff38aeb9cd879d859ce6e6eaa7d4
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-01-19 09:22:04 +0530

    Moved glade files to their own folder

--- linux_undervolt/constants.py
+++ linux_undervolt/constants.py
@@ -6,5 +6,6 @@ file_dir = os.path.dirname(os.path.abspath(__file__))
 CONFIG_DIR = os.path.join(home, ".config/linux-undervolt/")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "linux-undervolt.conf")
 
-MAIN_WINDOW = os.path.join(file_dir, "GUI.glade")
-ADVANCED_WINDOW = os.path.join(file_dir, "main_adv.glade")
+GLADE_FOLDER = os.path.join(file_dir, "glade_files/")
+MAIN_WINDOW = os.path.join(GLADE_FOLDER, "GUI.glade")
+ADVANCED_WINDOW = os.path.join(GLADE_FOLDER, "main_adv.glade")
