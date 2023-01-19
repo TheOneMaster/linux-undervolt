d4d3185d84427cf6fe8fdf34d077921a6f0ffbf8
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-01-19 16:30:39 +0530

    Updated constants

--- linux_undervolt/constants.py
+++ linux_undervolt/constants.py
@@ -1,11 +1,11 @@
 import os
 
-home = os.environ['HOME']
-file_dir = os.path.dirname(os.path.abspath(__file__))
+HOME = os.environ['HOME']
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-CONFIG_DIR = os.path.join(home, ".config/linux-undervolt/")
+CONFIG_DIR = os.path.join(HOME, ".config/linux-undervolt/")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "linux-undervolt.conf")
 
-GLADE_FOLDER = os.path.join(file_dir, "glade_files/")
+GLADE_FOLDER = os.path.join(FILE_DIR, "glade_files/")
 MAIN_WINDOW = os.path.join(GLADE_FOLDER, "GUI.glade")
 ADVANCED_WINDOW = os.path.join(GLADE_FOLDER, "main_adv.glade")
